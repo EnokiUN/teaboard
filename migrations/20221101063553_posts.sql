518220e5f8cabeeef834090e871ade9e33e0af75
Enoki <enokiun@gmail.com>
2022-11-01 10:23:06 +0300

    feat: update migrations

--- migrations/20221101063553_posts.sql
+++ migrations/20221101063553_posts.sql
@@ -2,11 +2,13 @@ CREATE TABLE IF NOT EXISTS posts (
 	id BIGINT PRIMARY KEY,
 	board VARCHAR(32) NOT NULL,
 	title VARCHAR(256) NOT NULL,
-	content VARCHAR(10000) NOT NULL,
+	content VARCHAR(10000),
 	pinned boolean NOT NULL DEFAULT false,
 	moderator boolean NOT NULL DEFAULT false,
 	locked boolean NOT NULL DEFAULT false,
+	parent BIGINT,
 	image BIGINT,
 	FOREIGN KEY (board) REFERENCES boards(id),
-	FOREIGN KEY (image) REFERENCES images(id)
+	FOREIGN KEY (image) REFERENCES images(id),
+	FOREIGN KEY (parent) REFERENCES posts(id)
 );
