4e881b8b4f5d0a89fb13cd4180c7ea5d3aa111c7
Enoki <enokiun@gmail.com>
2022-11-20 15:13:54 +0300

    feat(models): add support for fetching boards from the database

--- src/models/board.rs
+++ src/models/board.rs
@@ -1,7 +1,29 @@
+use rocket::{response::status::NotFound, serde::json::Json};
+use rocket_db_pools::sqlx::{pool::PoolConnection, MySql};
 use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Board {
     pub id: String,
     pub description: Option<String>,
+}
+
+impl Board {
+    pub async fn get(
+        id: String,
+        db: &mut PoolConnection<MySql>,
+    ) -> Result<Self, NotFound<Json<Value>>> {
+        sqlx::query_as!(
+            Board,
+            "
+SELECT *
+FROM boards
+WHERE id = ?",
+            id
+        )
+        .fetch_one(db)
+        .await
+        .map_err(|_| NotFound(Json(json!({"code": 404, "msg": "Unknown board"}))))
+    }
 }
