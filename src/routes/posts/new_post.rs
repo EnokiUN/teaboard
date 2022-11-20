c5b4f7e14302dc7787bc5daebc9da8ff63ac9d38
Enoki <enokiun@gmail.com>
2022-11-20 15:29:59 +0300

    feat(routes): add a new post route

---
+++ src/routes/posts/new_post.rs
@@ -0,0 +1,24 @@
+use rocket::{
+    form::Form, response::status::NotFound, serde::json::Json, tokio::sync::Mutex, State,
+};
+use rocket_db_pools::Connection;
+use serde_json::Value;
+
+use crate::{
+    id::IdGen,
+    models::{Board, Post, PostForm},
+    DB,
+};
+
+#[post("/<board>/new", data = "<post>")]
+pub async fn new<'a>(
+    board: String,
+    post: Form<PostForm<'a>>,
+    gen: &State<Mutex<IdGen>>,
+    mut db: Connection<DB>,
+) -> Result<Json<Post>, NotFound<Json<Value>>> {
+    let board = Board::get(board, &mut *db).await?;
+    Ok(Json(
+        Post::create(board, post.into_inner(), gen.inner(), &mut *db).await,
+    ))
+}
