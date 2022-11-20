ef9395240c7c403d6503c67dc5bef3c5785a6cdb
Enoki <enokiun@gmail.com>
2022-11-20 20:22:12 +0300

    feat(models): add a way to get a feed from a board

--- src/models/board.rs
+++ src/models/board.rs
@@ -3,6 +3,8 @@ use rocket_db_pools::sqlx::{pool::PoolConnection, MySql};
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
 
+use super::Post;
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Board {
     pub id: String,
@@ -11,7 +13,7 @@ pub struct Board {
 
 impl Board {
     pub async fn get(
-        id: String,
+        id: &str,
         db: &mut PoolConnection<MySql>,
     ) -> Result<Self, NotFound<Json<Value>>> {
         sqlx::query_as!(
@@ -25,5 +27,51 @@ WHERE id = ?",
         .fetch_one(db)
         .await
         .map_err(|_| NotFound(Json(json!({"code": 404, "msg": "Unknown board"}))))
+    }
+    pub async fn get_feed(
+        id: String,
+        before: Option<u64>,
+        limit: Option<u32>,
+        db: &mut PoolConnection<MySql>,
+    ) -> Result<Vec<Post>, NotFound<Json<Value>>> {
+        let board = Self::get(&id, db).await?;
+        let limit = limit.unwrap_or(10);
+        let posts = match before {
+            Some(before) => 
+                sqlx::query_as!(
+                    Post,
+                    r#"
+SELECT id, board, title, content, pinned as "pinned: _", moderator as "moderator: _", locked as "locked: _", parent, image
+FROM posts
+WHERE board = ?
+AND id < ?
+ORDER BY id DESC
+LIMIT ?
+                    "#,
+                    board.id,
+                    before,
+                    limit
+                    )
+                    .fetch_all(db)
+                    .await
+                    .unwrap(),
+            None =>
+                sqlx::query_as!(
+                    Post,
+                    r#"
+SELECT id, board, title, content, pinned as "pinned: _", moderator as "moderator: _", locked as "locked: _", parent, image
+FROM posts
+WHERE board = ?
+ORDER BY id DESC
+LIMIT ?
+                    "#,
+                    board.id,
+                    limit
+                    )
+                    .fetch_all(db)
+                    .await
+                    .unwrap(),
+        };
+        Ok(posts)
     }
 }
