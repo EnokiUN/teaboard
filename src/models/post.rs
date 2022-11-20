361ae6ae09d5fb95d8d0a835677ffe81a6c29baf
Enoki <enokiun@gmail.com>
2022-11-20 15:27:35 +0300

    feat(models): add a way create posts

--- src/models/post.rs
+++ src/models/post.rs
@@ -1,4 +1,12 @@
+use rocket::{fs::TempFile, response::status::NotFound, serde::json::Json};
+use rocket_db_pools::sqlx::{pool::PoolConnection, MySql};
 use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
+use tokio::sync::Mutex;
+
+use crate::id::IdGen;
+
+use super::Board;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Post {
@@ -24,4 +32,44 @@ pub struct PostJson {
 pub struct PostForm<'a> {
     pub post: Json<PostJson>,
     pub image: Option<TempFile<'a>>,
+}
+
+impl Post {
+    pub async fn create<'a>(
+        board: Board,
+        form: PostForm<'a>,
+        gen: &Mutex<IdGen>,
+        db: &mut PoolConnection<MySql>,
+    ) -> Self {
+        let _image = form.image;
+        let post = form.post.into_inner();
+        let id = gen.lock().await.generate();
+        sqlx::query!(
+            "
+INSERT INTO posts(id, board, title, content, parent, image)
+VALUES(?, ?, ?, ?, ?, ?)
+            ",
+            id,
+            board.id,
+            post.title,
+            post.content,
+            post.parent,
+            None::<u32>
+        )
+        .execute(db)
+        .await
+        .unwrap();
+        Self {
+            id,
+            board: board.id,
+            title: post.title,
+            content: post.content,
+            pinned: false,
+            moderator: false,
+            locked: false,
+            parent: post.parent,
+            image: None,
+        }
+    }
+
 }
