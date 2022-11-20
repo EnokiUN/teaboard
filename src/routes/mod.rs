f1ce05e23f0100f92545ea74373a3c1254d8cc1d
Enoki <enokiun@gmail.com>
2022-11-20 20:25:13 +0300

    feat(routes): add new routes

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,7 +1,8 @@
 use rocket::{routes, Route};
 
+mod boards;
 mod posts;
 
 pub fn routes() -> Vec<Route> {
-    routes![posts::new, posts::get]
+    routes![posts::new, posts::get, boards::info, boards::feed]
 }
