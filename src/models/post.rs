1d91b65ebd793d9855b1981bad1365b30d027fca
Enoki <enokiun@gmail.com>
2022-11-20 15:27:00 +0300

    feat(models): add PostForm and PostJson

--- src/models/post.rs
+++ src/models/post.rs
@@ -12,3 +12,16 @@ pub struct Post {
     pub parent: Option<u64>,
     pub image: Option<u64>,
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PostJson {
+    pub title: String,
+    pub content: Option<String>,
+    pub parent: Option<u64>,
+}
+
+#[derive(Debug, FromForm)]
+pub struct PostForm<'a> {
+    pub post: Json<PostJson>,
+    pub image: Option<TempFile<'a>>,
+}
