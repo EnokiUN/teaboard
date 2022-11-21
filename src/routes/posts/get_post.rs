da4fbcdad28c8c7ac903b751e7725c405597aca2
Enoki <enokiun@gmail.com>
2022-11-21 22:02:59 +0300

    feat(routes): use PostInfo when fetching a post

--- src/routes/posts/get_post.rs
+++ src/routes/posts/get_post.rs
@@ -2,9 +2,12 @@ use rocket::{response::status::NotFound, serde::json::Json};
 use rocket_db_pools::Connection;
 use serde_json::Value;
 
-use crate::{models::Post, DB};
+use crate::{
+    models::{Post, PostInfo},
+    DB,
+};
 
 #[get("/posts/<id>")]
-pub async fn get(id: u64, mut db: Connection<DB>) -> Result<Json<Post>, NotFound<Json<Value>>> {
-    Post::get(id, &mut *db).await.map(|p| Json(p))
+pub async fn get(id: u64, mut db: Connection<DB>) -> Result<Json<PostInfo>, NotFound<Json<Value>>> {
+    Post::info(id, &mut *db).await.map(|p| Json(p))
 }
