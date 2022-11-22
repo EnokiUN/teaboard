d98baf4e59dfb31bc4b55f370d7346e76032ffc9
Enoki <enokiun@gmail.com>
2022-11-22 23:09:11 +0300

    feat(routes): export the new /images route

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,8 +1,15 @@
 use rocket::{routes, Route};
 
 mod boards;
+mod fetch_image;
 mod posts;
 
 pub fn routes() -> Vec<Route> {
-    routes![posts::new, posts::get, boards::info, boards::feed]
+    routes![
+        posts::new,
+        posts::get,
+        boards::info,
+        boards::feed,
+        fetch_image::fetch_image
+    ]
 }
