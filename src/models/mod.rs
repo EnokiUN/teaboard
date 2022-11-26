b11a24067215b2a11dd5afa37de23bc4657e56c0
Enoki <enokiun@gmail.com>
2022-11-26 20:29:25 +0300

    feat(models): add new InstanceInfo model

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -2,6 +2,16 @@ mod board;
 mod image;
 mod post;
 
+use crate::conf::Conf;
+
 pub use self::image::{FetchResponse, Image};
 pub use board::Board;
 pub use post::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct InstanceInfo {
+    #[serde(flatten)]
+    pub info: Conf,
+    pub boards: Vec<Board>,
+}
