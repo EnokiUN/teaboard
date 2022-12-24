7918cb3c90164754fb4499a6c9d49611227f775e
Enoki <enokiun@gmail.com>
2022-12-24 21:45:48 +0300

    feat: implement simple password auth request guards

---
+++ src/auth.rs
@@ -0,0 +1,64 @@
+use rocket::{
+    http::Status,
+    request::{FromRequest, Outcome, Request},
+};
+use serde::{Deserialize, Serialize};
+
+use crate::conf::Conf;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PasswordAuth {
+    pub admin: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct StrictPasswordAuth;
+
+#[derive(Debug)]
+pub enum AuthError {
+    Unauthorised,
+    AuthNotFound,
+}
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for PasswordAuth {
+    type Error = AuthError;
+
+    async fn from_request(req: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        match req.headers().get_one("Authorization") {
+            Some(password) => Outcome::Success(PasswordAuth {
+                admin: password
+                    == &req
+                        .rocket()
+                        .state::<Conf>()
+                        .expect("Could not find instance config in rocket state")
+                        .password,
+            }),
+            None => Outcome::Failure((Status::BadRequest, AuthError::AuthNotFound)),
+        }
+    }
+}
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for StrictPasswordAuth {
+    type Error = AuthError;
+
+    async fn from_request(req: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        match req.headers().get_one("Authorization") {
+            Some(password) => {
+                if password
+                    == &req
+                        .rocket()
+                        .state::<Conf>()
+                        .expect("Could not find instance config in rocket state")
+                        .password
+                {
+                    Outcome::Success(StrictPasswordAuth)
+                } else {
+                    Outcome::Failure((Status::Forbidden, AuthError::Unauthorised))
+                }
+            }
+            None => Outcome::Failure((Status::BadRequest, AuthError::AuthNotFound)),
+        }
+    }
+}
