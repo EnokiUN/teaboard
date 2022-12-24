e9957627c408ae4a3352d1fcf3dd5719493a5e7d
Enoki <enokiun@gmail.com>
2022-12-24 20:54:10 +0300

    refactor: improve config errors

--- src/conf.rs
+++ src/conf.rs
@@ -1,6 +1,6 @@
 use std::{env, fs};
 
-use anyhow::{anyhow, Context};
+use anyhow::{bail, Context};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -16,21 +16,17 @@ impl Conf {
             fs::read_to_string(env::var("CONFIG_PATH").unwrap_or_else(|_| "Tea.toml".to_string()))?;
         let conf: Conf = toml::from_str(&conf).context("Could not deserialize toml file")?;
         if conf.name.is_empty() {
-            Err(anyhow!("Instance name cannot be empty"))?
+            bail!("Instance name cannot be empty");
         }
         if conf.name.len() > 32 {
-            Err(anyhow!(
-                "Instance name cannot be more than 32 characters long"
-            ))?
+            bail!("Instance name cannot be more than 32 characters long");
         }
         if let Some(description) = &conf.description {
             if description.is_empty() {
-                Err(anyhow!("Instance description cannot be empty"))?
+                bail!("Instance description cannot be empty");
             }
             if description.len() > 2000 {
-                Err(anyhow!(
-                    "Instance description cannot be more than 2000 characters long"
-                ))?
+                bail!("Instance description cannot be more than 2000 characters long");
             }
         }
         Ok(conf)
